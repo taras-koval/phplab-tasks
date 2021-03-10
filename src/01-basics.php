87f1f7f475cffd58610d5cb71260fa1b52e87d6c
Taras <tkoval37@gmail.com>
2021-03-10 20:22:23 +0200

    fix: isLeapYear

--- src/01-basics.php
+++ src/01-basics.php
@@ -44,15 +44,7 @@ function isLeapYear(int $year)
         throw new InvalidArgumentException;
     }
 
-    if ($year % 4 !== 0) {
-        return false;
-    } else if ($year % 100 !== 0) {
-        return true;
-    } else if ($year % 400 !== 0) { 
-        return false;
-    } else {
-        return true;
-    }
+    return $year % 400 == 0 || ($year % 100 != 0 && $year % 4 == 0);
 }
 
 /**
